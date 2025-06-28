42a9153d7de2af3fb8891d3fee8af70a896a3456
kaa-dan <nithinraj.dev@gmail.com>
2025-06-28 19:16:37 +0530

    env-config

---
+++ cmd/api/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/kaa-dan/clean-architecture-go/internal/config"
+
+func main() {
+	// Load configuration
+
+	cfg := config.Load()
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,13 @@
 package config
+
+type Config struct {
+	Environment    string
+	Port           string
+	DatabaseURL    string
+	DatabaseName   string
+	JWTSecret      string
+	JWTExpiryHours int
+	LogLevel       string
+	RateLimitRPM   int
+	BCryptCost     int
+}

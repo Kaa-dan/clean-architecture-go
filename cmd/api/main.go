6a21c80dfc889dcd38a7a7df0c7e0d773d61fbd4
kaa-dan <nithinraj.dev@gmail.com>
2025-06-29 17:36:52 +0530

    logger setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/kaa-dan/clean-architecture-go/internal/config"
+import (
+	"github.com/kaa-dan/clean-architecture-go/internal/config"
+	"github.com/kaa-dan/clean-architecture-go/pkg/logger"
+)
 
 func main() {
 	// Load configuration
 
 	cfg := config.Load()
+
+	// Initialize logger
+	logger.Init(cfg.LogLevel)
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,60 @@
 package logger
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func Init(level string) {
+	log = logrus.New()
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	logLevel, err := logrus.ParseLevel(level)
+	if err != nil {
+		logLevel = logrus.InfoLevel
+	}
+	log.SetLevel(logLevel)
+}
+
+func GetLogger() *logrus.Logger {
+	if log == nil {
+		Init("info")
+	}
+	return log
+}
+
+func Info(args ...interface{}) {
+	GetLogger().Info(args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	GetLogger().Infof(format, args...)
+}
+
+func Error(args ...interface{}) {
+	GetLogger().Error(args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	GetLogger().Errorf(format, args...)
+}
+
+func Debug(args ...interface{}) {
+	GetLogger().Debug(args...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	GetLogger().Debugf(format, args...)
+}
+
+func Warn(args ...interface{}) {
+	GetLogger().Warn(args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	GetLogger().Warnf(format, args...)
+}

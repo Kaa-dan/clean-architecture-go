ba041028f11917322ce6f3e9c006f62a2d7e0611
kaa-dan <nithinraj.dev@gmail.com>
2025-06-29 18:08:45 +0530

    added mogo db db connection configurations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
+
 	"github.com/kaa-dan/clean-architecture-go/internal/config"
+	"github.com/kaa-dan/clean-architecture-go/internal/infrastructure/database"
 	"github.com/kaa-dan/clean-architecture-go/pkg/logger"
 )
 
@@ -12,4 +15,11 @@ func main() {
 
 	// Initialize logger
 	logger.Init(cfg.LogLevel)
+
+	//Connect to MongoDb
+
+	db, err := database.NewMongoDB(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 }
--- internal/infrastructure/database/mongodb.go
+++ internal/infrastructure/database/mongodb.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewMongoDB(uri string) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(uri)
+	clientOptions.SetMaxPoolSize(100)
+	clientOptions.SetMinPoolSize(5)
+	clientOptions.SetMaxConnIdleTime(30 * time.Second)
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping the database
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
